1a13b5ff965d3afb1dd0d6b3582125f60777c3c7
Anel <abraranel@gmail.com>
2022-06-21 14:08:31 +0600

    add solution for task 3

--- index.js
+++ index.js
@@ -22,3 +22,17 @@ function userName(person) {
     return person.name === 'Bob';
 }
 console.log(user.find(userName));
+
+//3
+let city1 = {};
+city1.name = 'ГородN';
+city1.population = 1000000;
+
+let city2 = {
+    name: "ГородM", 
+    population: 1e6
+}
+
+city1.getName = city2.getName = function () { return this.name; };
+console.log(city1.getName());
+console.log(city2.getName());
